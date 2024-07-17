95502578f3302ae76e753a8c9092562fc8c3ea00
HANANEZOUBI <hananezoubi1@gmail.com>
2024-07-16 17:54:34 -0700

    advanced

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' 101-students.py
+'''
+
+
+def top_students(mongo_collection):
+    '''Prints all students in a collection sorted by average score.
+    '''
---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+''' 102-log_stats.py
+'''
